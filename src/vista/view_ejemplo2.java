15f356c9f2b457364cc69f46510a43df9e7d897a
Daniel <ddiaz@ups.edu.ec>
2024-05-27 15:37:29 -0500

    Commit comentarios

--- src/vista/view_ejemplo2.java
+++ src/vista/view_ejemplo2.java
@@ -14,7 +14,8 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 
 public class view_ejemplo2 extends JFrame {
-
+	
+	//Declarar Variable globales
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	public JTextField txt_cadena;
@@ -29,7 +30,9 @@ public class view_ejemplo2 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					//Crear un nuevo contenedor JFrame
 					view_ejemplo2 frame = new view_ejemplo2();
+					//Mostrar la ventana
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,24 +45,33 @@ public class view_ejemplo2 extends JFrame {
 	 * Create the frame.
 	 */
 	public view_ejemplo2() {
+		//Terminar el programa cuando el usuario cierre la ventana
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Asignar el tamaño inicial de la ventana
 		setBounds(100, 100, 450, 194);
+		//Crear contener para los componentes 
 		contentPane = new JPanel();
+		//Determinar el brode del panel
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		//Agregar el componente dentro del Frame
 		setContentPane(contentPane);
+		//Ulitilizar el layout como null para colocar en cualquier parte los componentes 
 		contentPane.setLayout(null);
-		
+		//Crear la etiqueta 
 		lbl_etiqueta1 = new JLabel("Texto:");
+		//Definir la tamaño y la fuente de la etiqueta
 		lbl_etiqueta1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		//Asiganta la posicion en X y Y dentro del panel y su ancho y alto
+		//componente.setBounds(PosicionX, PosicionY, Ancho, Alto)
 		lbl_etiqueta1.setBounds(31, 36, 45, 13);
+		//Añadir el componente al panel principal 
 		contentPane.add(lbl_etiqueta1);
-		
+		//Crear un campo de texto
 		txt_cadena = new JTextField();
 		txt_cadena.setBounds(86, 25, 226, 29);
 		contentPane.add(txt_cadena);
 		txt_cadena.setColumns(10);
-		
+		//Crear boton 
 		btn_ok = new JButton("OK");
 		btn_ok.setBounds(322, 25, 63, 34);
 		contentPane.add(btn_ok);
